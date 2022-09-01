1573bbb553b59226cd8479699dc5031f0a11f140
Robert Clarke <clarke.john.robert@gmail.com>
2022-09-01 14:21:15 -0400

    Changed script to output results to stdout

--- header_generator.sh
+++ header_generator.sh
@@ -8,9 +8,9 @@ HEADER_GUARD_LABEL="$2"
 [[ -z "$SOURCE_FILE" ]] && echo "$0 ERROR: Must specify target .c file" && exit
 [[ -z "$HEADER_GUARD_LABEL" ]] && echo "$0 ERROR: Must specify header guard label" && exit
 
-echo "//Generated-file" > "$GENERATED_FILE"
-echo "#ifndef $HEADER_GUARD_LABEL" >> "$GENERATED_FILE"
-echo "#define $HEADER_GUARD_LABEL" >> "$GENERATED_FILE"
-grep -E "^#include" "$SOURCE_FILE" | grep -Ev "$GENERATED_FILE" >> "$GENERATED_FILE"
-grep -E "(.* {)" "$SOURCE_FILE" | grep -Ev "if|else|while|for|switch" | sed 's/ {/;/' >> "$GENERATED_FILE"
-echo "#endif" >> "$GENERATED_FILE"
+echo "//Generated-file"
+echo "#ifndef $HEADER_GUARD_LABEL"
+echo "#define $HEADER_GUARD_LABEL"
+grep -E "^#include" "$SOURCE_FILE" | grep -Ev "$GENERATED_FILE"
+grep -E "(.* {)" "$SOURCE_FILE" | grep -Ev "if|else|while|for|switch" | sed 's/ {/;/'
+echo "#endif"
