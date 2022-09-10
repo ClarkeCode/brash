eb2c2f7dbae63b70f334c31956d00fb674ef87be
Robert Clarke <clarke.john.robert@gmail.com>
2022-09-10 02:34:37 -0400

    Implemented interpretation for arithmetic operators, logical operators, comparison/equality operators (for numbers)

--- main.c
+++ main.c
@@ -3,12 +3,24 @@
 #include <stdbool.h> //bool
 #include <string.h>
 #include <assert.h> //assert
+#include <math.h> //fmod
 
 #include "structs.h"
 #include "enumerations.h"
 #include "enum_lookups.h"
 #include "lexer.h"
 #include "parser_ast.h"
+
+void dump_value(FILE* fp, Value* val) {
+		if (val->type == TYPE_NUMBER)
+			fprintf(fp, "%-10s %f", "NUMBER",  val->as.number);
+		else if (val->type == TYPE_BOOLEAN)
+			fprintf(fp, "%-10s %s", "BOOLEAN", val->as.boolean ? "true" : "false");
+		else if (val->type == TYPE_STRING)
+			fprintf(fp, "%-10s %s", "STRING",  val->as.string);
+		else
+			fprintf(fp, "%-10s", "NON-VALUE");
+}
 
 typedef struct {
 	Value* _stack;
@@ -29,41 +41,169 @@ void free_interpreter(Interpreter* terp) {
 }
 void dump_interpreter(FILE* fp, Interpreter* terp) {
 	for (size_t x = 0; x < terp->_top_index; x++) {
-		if ((terp->_stack + x)->type == TYPE_NUMBER)
-			fprintf(fp, "%3d %-10s %f\n", x, "NUMBER",  (terp->_stack + x)->as.number);
-		else if ((terp->_stack + x)->type == TYPE_BOOLEAN)
-			fprintf(fp, "%3d %-10s %s\n", x, "BOOLEAN", (terp->_stack + x)->as.boolean ? "true" : "false");
-		else if ((terp->_stack + x)->type == TYPE_STRING)
-			fprintf(fp, "%3d %-10s %s\n", x, "STRING",  (terp->_stack + x)->as.string);
-		else
-			fprintf(fp, "%3d %-10s\n", x, "NON-VALUE");
+		fprintf(fp, "%3d ", x);
+		dump_value(fp, terp->_stack + x);
+		fprintf(fp, "\n");
 	}
 }
 
-Value pop(Interpreter* terp) {
+Value pop(Interpreter* terp, FILE* fp) {
 	if (terp->_top_index - 1 == (size_t)-1) {fprintf(stderr, "ERR: Stack Underflow\n");} //Stack underflow
+	if (fp) {
+		fprintf(fp, "POP:  ");
+		dump_value(fp, terp->_stack + (terp->_top_index)-1);
+		fprintf(fp, "\n");
+	}
 	return terp->_stack[--(terp->_top_index)];
 }
-void push(Interpreter* terp, Value val) {
+void push(Interpreter* terp, Value val, FILE* fp) {
 	if (terp->_top_index + 1 >= terp->_max_index) {fprintf(stderr, "ERR: Stack Overflow\n");} //Stack overflow
+	if (fp) {
+		fprintf(fp, "PUSH: ");
+		dump_value(fp, &val);
+		fprintf(fp, "\n");
+	}
 	terp->_stack[(terp->_top_index)++] = val; //Copy
 }
 
-void interpret(Interpreter* terp, ASTNode* node) {
+void interpret(Interpreter* terp, ASTNode* node, FILE* fp) {
 	if (!node) return;
 	if (node->value.type != TYPE_NOT_VALUE) {
-		push(terp, node->value);
+		push(terp, node->value, fp);
 	}
 
-	interpret(terp, node->left);
-	interpret(terp, node->right);
+	interpret(terp, node->left, fp);
+	interpret(terp, node->right, fp);
+
+	switch (node->token->type) {
+		case OPERATOR_ADD: {
+			Value rval = pop(terp, fp);
+			Value lval = pop(terp, fp);
+			Value nval = {TYPE_NUMBER, {.number=(lval.as.number + rval.as.number)}};
+			fprintf(fp, "ADD\n");
+			push(terp, nval, fp);
+		} break;
+
+		case OPERATOR_SUB: {
+			Value rval = pop(terp, fp);
+			Value lval = pop(terp, fp);
+			Value nval = {TYPE_NUMBER, {.number=(lval.as.number - rval.as.number)}};
+			fprintf(fp, "SUBTRACT\n");
+			push(terp, nval, fp);
+		} break;
+
+		case OPERATOR_MULTIPLY: {
+			Value rval = pop(terp, fp);
+			Value lval = pop(terp, fp);
+			Value nval = {TYPE_NUMBER, {.number=(lval.as.number * rval.as.number)}};
+			fprintf(fp, "MULTIPLY\n");
+			push(terp, nval, fp);
+		} break;
+
+		case OPERATOR_DIVIDE: {
+			Value rval = pop(terp, fp);
+			Value lval = pop(terp, fp);
+			Value nval = {TYPE_NUMBER, {.number=(lval.as.number / rval.as.number)}};
+			fprintf(fp, "DIVIDE\n");
+			push(terp, nval, fp);
+		} break;
+
+		case OPERATOR_MODULO: {
+			Value rval = pop(terp, fp);
+			Value lval = pop(terp, fp);
+			Value nval = {TYPE_NUMBER, {.number=(fmod(lval.as.number, rval.as.number))}};
+			fprintf(fp, "MODULO\n");
+			push(terp, nval, fp);
+		} break;
+
+		case OPERATOR_EQUALITY: { //TODO: define equality for bool and string?
+			Value rval = pop(terp, fp);
+			Value lval = pop(terp, fp);
+			Value nval = {TYPE_BOOLEAN, {.boolean=(lval.as.number == rval.as.number)}};
+			fprintf(fp, "EQUALITY\n");
+			push(terp, nval, fp);
+		} break;
+
+		case OPERATOR_INEQUALITY: { //TODO: define equality for bool and string?
+			Value rval = pop(terp, fp);
+			Value lval = pop(terp, fp);
+			Value nval = {TYPE_BOOLEAN, {.boolean=(lval.as.number != rval.as.number)}};
+			fprintf(fp, "INEQUALITY\n");
+			push(terp, nval, fp);
+		} break;
+
+		case OPERATOR_LESSER: {
+			Value rval = pop(terp, fp);
+			Value lval = pop(terp, fp);
+			Value nval = {TYPE_BOOLEAN, {.boolean=(lval.as.number < rval.as.number)}};
+			fprintf(fp, "LESSER\n");
+			push(terp, nval, fp);
+		} break;
+
+		case OPERATOR_GREATER: {
+			Value rval = pop(terp, fp);
+			Value lval = pop(terp, fp);
+			Value nval = {TYPE_BOOLEAN, {.boolean=(lval.as.number > rval.as.number)}};
+			fprintf(fp, "GREATER\n");
+			push(terp, nval, fp);
+		} break;
+
+		case OPERATOR_LESSER_EQUAL: {
+			Value rval = pop(terp, fp);
+			Value lval = pop(terp, fp);
+			Value nval = {TYPE_BOOLEAN, {.boolean=(lval.as.number <= rval.as.number)}};
+			fprintf(fp, "LESSER_EQUAL\n");
+			push(terp, nval, fp);
+		} break;
+
+		case OPERATOR_GREATER_EQUAL: {
+			Value rval = pop(terp, fp);
+			Value lval = pop(terp, fp);
+			Value nval = {TYPE_BOOLEAN, {.boolean=(lval.as.number >= rval.as.number)}};
+			fprintf(fp, "GREATER_EQUAL\n");
+			push(terp, nval, fp);
+		} break;
+
+		case LOGICAL_AND: {
+			Value rval = pop(terp, fp);
+			Value lval = pop(terp, fp);
+			Value nval = {TYPE_BOOLEAN, {.boolean=(lval.as.boolean && rval.as.boolean)}};
+			fprintf(fp, "LOGICAL_AND\n");
+			push(terp, nval, fp);
+		} break;
+
+		case LOGICAL_OR: {
+			Value rval = pop(terp, fp);
+			Value lval = pop(terp, fp);
+			Value nval = {TYPE_BOOLEAN, {.boolean=(lval.as.boolean || rval.as.boolean)}};
+			fprintf(fp, "LOGICAL_OR\n");
+			push(terp, nval, fp);
+		} break;
+
+		case LOGICAL_XOR: {
+			Value rval = pop(terp, fp);
+			Value lval = pop(terp, fp);
+			Value nval = {TYPE_BOOLEAN, {.boolean=(lval.as.boolean ^  rval.as.boolean)}};
+			fprintf(fp, "LOGICAL_XOR\n");
+			push(terp, nval, fp);
+		} break;
+
+		case LOGICAL_NOT: {
+			Value rval = pop(terp, fp);
+			Value nval = {TYPE_BOOLEAN, {.boolean=(!rval.as.boolean)}};
+			fprintf(fp, "LOGICAL_NOT\n");
+			push(terp, nval, fp);
+		} break;
+	}
 }
 
 int main(int argc, char* argv[]) {
 
 	char* program;
 	if (argc > 1) { program = argv[1]; }
-	else          { program = "greg = !false ^^ 1+2*3-9 >= 4*5 && true"; }
+	else          { program = "greg = !false ^^ 1+2/3-9 >= 4*5 && true"; }
+
+	fprintf(stdout, "Program: %s\n\n", program);
 	
 	Lexer* lex = make_lexer(program, "notfile", 0);
 
@@ -71,9 +211,10 @@ int main(int argc, char* argv[]) {
 	Token* tok = lexer_tokenize_all(lex, &num_tok);
 	{
 		fprintf(stdout, "=== LEXER ===\n");
-		FILE* outfile = fopen("lexer.out", "wb");
 		dump_lexer_tokens(stdout, tok, num_tok);
 		fprintf(stdout, "Remaining in lexer: '%s'\n", lex->line + lex->char_offset);
+
+		FILE* outfile = fopen("lexer.out", "wb");
 		dump_lexer_tokens(outfile, tok, num_tok);
 		fclose(outfile);
 	}
@@ -82,15 +223,19 @@ int main(int argc, char* argv[]) {
 	ASTNode* ast = parse_ast(parser, -1);
 	{
 		FILE* fp = fopen("ast.out", "w");
+		fprintf(fp, "\n=== PARSER ===\n");
+		dump_tree(fp, ast);
+		fclose(fp);
+
 		fprintf(stdout, "\n=== PARSER ===\n");
 		dump_tree(stdout, ast);
-		fclose(fp);
 	}
 
 	Interpreter* terp = make_interpreter();
 	{
 		fprintf(stdout, "\n=== INTERPRETER ===\n");
-		interpret(terp, ast);
+		interpret(terp, ast, stdout);
+		fprintf(stdout, "\nInterpreter value stack final state:\n");
 		dump_interpreter(stdout, terp);
 	}
 
