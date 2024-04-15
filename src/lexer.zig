484dc9018229c200febda6f8cfbc4d24c5c3f637
ClarkeCode <clarke.john.robert@gmail.com>
2024-04-15 00:46:36 -0400

    Preliminary start at lexing!

--- src/lexer.zig
+++ src/lexer.zig
@@ -58,28 +58,69 @@ pub const Token = struct {
 
 	//TODO: This probably leaks memory, but is good enough for now
 	pub fn toString(self: Token) []const u8 {
-		return std.fmt.allocPrint(std.heap.page_allocator, "Token <{s}> '{s}'" ++ "aaa", .{self.kind.toString(), self.value}) catch "";
+		return std.fmt.allocPrint(std.heap.page_allocator, "Token <{s}> '{s}'", .{self.kind.toString(), self.value}) catch "";
 	}
 
-	pub fn isType(self: Token, tkTypes: []const TokenType) bool {
+	pub fn isType(self: Token, kind1: TokenType) bool { return self.kind == kind1; }
+	pub fn isType2(self: Token, kind1: TokenType, kind2: TokenType) bool { return self.kind == kind1 or self.kind == kind2; }
+	pub fn isType3(self: Token, kind1: TokenType, kind2: TokenType, kind3: TokenType) bool { return self.kind == kind1 or self.kind == kind2 or self.kind == kind3; }
+	pub fn isType4(self: Token, kind1: TokenType, kind2: TokenType, kind3: TokenType, kind4: TokenType) bool { return self.isType3(kind1, kind2, kind3) or self.kind == kind4; }
+	pub fn isTypes(self: Token, tkTypes: []const TokenType) bool {
 		for (tkTypes) |elem| {
 			if (self.kind == elem) return true;
 		}
 		return false;
 	}
 };
+pub fn makeToken(kind: TokenType, value: []const u8) Token {
+	return Token{.kind = kind, .value = value};
+}
 
 
-const Lexer = struct {
-	tokens: []Token,
-	source: []u8,
-	position: u64,
+
+fn streq(a: []const u8, b: []const u8) bool {
+	return std.mem.eql(u8, a, b);
+}
+
+const TokenList_t = std.ArrayList(Token);
+pub const Lexer = struct {
+	tokens: TokenList_t,
+	source: []const u8,
+	position: usize = 0,
+
+	fn advanceN(self: *Lexer, advance: u64) void { self.position += advance; }
+	fn getRemainder(self: *Lexer) []const u8 { return self.source[self.position..]; }
+	fn atEOF(self: *Lexer) bool { return self.position >= self.source.len; }
+
+	pub fn deinit(self: *Lexer) void { self.tokens.deinit(); }
 };
 
-pub fn Tokenize(source: []const u8) []Token {
-	var lex: Lexer = .{.source = source, .position = 0};
+fn hasBasicMatch(needle: []const u8, haystack: []const u8) bool {
+	return if (needle.len <= haystack.len) streq(needle, haystack[0..needle.len]) else false;
+}
 
-	return lex.tokens;
+pub fn Tokenize(lexer: *Lexer) !void {
+	while (!lexer.atEOF()) {
+		const remainder: []const u8 = lexer.getRemainder();
+		
+		if (hasBasicMatch("==", remainder)) {
+			try lexer.tokens.append(makeToken(.Equality, "=="));
+			lexer.advanceN(2);
+		}
+		else if (hasBasicMatch("+", remainder)) {
+			try lexer.tokens.append(makeToken(.Add, "+"));
+			lexer.advanceN(1);
+		}
+		else if (hasBasicMatch("1", remainder) or hasBasicMatch("2", remainder) or hasBasicMatch("3", remainder)) {
+			try lexer.tokens.append(makeToken(.Number, remainder[0..1]));
+			lexer.advanceN(1);
+		}
+	}
+}
+
+pub fn InitLexer(allocator: std.mem.Allocator, source: []const u8) Lexer {
+	var lex: Lexer = .{.tokens = TokenList_t.init(allocator), .source = source, .position = 0};
+	return lex;
 }
 
 
--- src/main.zig
+++ src/main.zig
@@ -7,6 +7,38 @@ pub fn streq(a: []const u8, b: []const u8) bool {
 const lexer = @import("lexer.zig");
 const TokenType = lexer.TokenType;
 
+const Colour = enum { Red, Green, Blue };
+
+pub fn variad(first: Colour, args: anytype) !void {
+	std.debug.print("BBBBBBBBBBB\n", .{});
+	std.debug.print("{}\n", .{first});
+	std.debug.print("{}\n", .{args});
+
+	const ArgType = @TypeOf(args);
+	const argTypeInfo = @typeInfo(ArgType);
+	const fieldsInfo = argTypeInfo.Struct.fields;
+	
+	// const TI = @typeInfo(ArgType);
+	// comptime var FieldInfo = TI.Struct.fields;
+	std.debug.print("{}\n", .{ArgType});
+	std.debug.print("{}\n", .{fieldsInfo.len});
+	std.debug.print("{}\n", .{fieldsInfo[0]});
+	std.debug.print("~{}\n", .{fieldsInfo[0].type});
+
+	std.debug.print("-------------------\n", .{});
+	inline for (0..fieldsInfo.len) |index| {
+		std.debug.print("{}\n", .{index});
+		std.debug.print("{}\n", .{fieldsInfo[index]});
+		std.debug.print("{}\n", .{fieldsInfo[index].type});
+		// std.debug.print("{?}\n", .{fieldsInfo[index]});
+		// std.debug.print("{}\n", .{});
+	}
+
+	// std.debug.print("{any}\n", .{TI.Struct.fields});
+	// std.debug.print("{any}\n", .{FieldInfo});
+	std.debug.print("-------------------\n", .{});
+}
+
 pub fn main() !void {
 	// Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
 	std.debug.print("Time to be {s}!\n", .{"brash"});
@@ -16,17 +48,30 @@ pub fn main() !void {
 	// stdout, not any debugging messages.
 	const stdout_file = std.io.getStdOut().writer();
 	var bw = std.io.bufferedWriter(stdout_file);
-	const stdout = bw.writer();
+	const stdout = bw.writer(); 
 
-	var aa: lexer.Token = .{.kind = TokenType.Equality, .value = "=="};
-	try stdout.print("{s}\n", .{aa.toString()});
-	try stdout.print("is eq {} {}\n", .{
-		aa.isType(&[_]TokenType{.Add, .Subtract}),
-		aa.isType(&[_]TokenType{.And, .Equality})
-	});
 
-	try stdout.print("{}.{}\n", .{streq("aa", "be"), streq("aa", "aa")});
-	try stdout.print("AAAAA.\n", .{});
+	var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+	var alloc = gpa.allocator();
+
+	var lex = lexer.InitLexer(alloc, "1+2==3");
+	defer lex.deinit();
+	// try stdout.print("{}\n", .{lex.atEOF()});
+	try stdout.print("LEXER\n", .{});
+	try lexer.Tokenize(&lex);
+	for (lex.tokens.items) |token| {
+		try stdout.print("{s}\n", .{token.toString()});
+	}
+
+	// var aa: lexer.Token = lexer.makeToken(TokenType.Equality, "==");
+	// try stdout.print("{s}\n", .{aa.toString()});
+	// try stdout.print("is eq {} {}\n", .{
+	// 	aa.isTypes(&[_]TokenType{.Add, .Subtract}),
+	// 	aa.isTypes(&[_]TokenType{.And, .Equality})
+	// });
+
+	// try stdout.print("{}.{}\n", .{streq("aa", "aabe"[0..2]), streq("aa", "aa")});
+	// try stdout.print("AAAAA.\n", .{});
 	try stdout.print("Run `zig build test` to run the tests.\n", .{});
 
 	try bw.flush(); // don't forget to flush!
@@ -37,4 +82,19 @@ test "simple test" {
 	defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
 	try list.append(42);
 	try std.testing.expectEqual(@as(i32, 42), list.pop());
+}
+
+test "trying arraylist" {
+	var list = std.ArrayList(i32).init(std.testing.allocator);
+	defer list.deinit();
+	try list.append(234);
+	try list.append(111);
+	try list.append(567);
+
+	try std.testing.expectEqualSlices(i32, &[_]i32{234, 111, 567}, list.items);
+
+	try std.testing.expectEqual(@as(i32, 111), list.orderedRemove(1));
+	try std.testing.expectEqual(@as(i32, 567), list.getLast());
+	try std.testing.expectEqual(@as(i32, 567), list.pop());
+	try std.testing.expectEqual(@as(i32, 234), list.pop());
 }
